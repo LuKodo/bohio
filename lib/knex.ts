878e7668357f86363a3d871065772f873907d141
Luis Caraballo <lcm7039@gmail.com>
2025-06-29 18:31:08 -0500

    last revision

--- lib/knex.ts
+++ lib/knex.ts
@@ -4,11 +4,11 @@ import "dotenv/config"
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "159.89.155.218",
     port: Number(process.env.DB_PORT) || 5432,
-    user: process.env.DB_USER || "hdm",
-    password: process.env.DB_PASSWORD || "helpdesk*2025",
-    database: process.env.DB_NAME || "bohio",
+    user: process.env.DB_USER || "external",
+    password: process.env.DB_PASSWORD || "bohio2025",
+    database: process.env.DB_NAME || "inmobiliariabohio.cloudpepper.site",
     ssl: false,
   },
   useNullAsDefault: true,
