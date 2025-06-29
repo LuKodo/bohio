8c4cb96866ed5c9fc4fed5b7b0ad4d9437c1c4cf
Luis Caraballo <lcm7039@gmail.com>
2025-06-29 18:38:22 -0500

    feat: eliminar valores predeterminados en la configuración de conexión de la base de datos

--- lib/knex.ts
+++ lib/knex.ts
@@ -4,11 +4,11 @@ import "dotenv/config"
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DB_HOST || "159.89.155.218",
-    port: Number(process.env.DB_PORT) || 5432,
-    user: process.env.DB_USER || "external",
-    password: process.env.DB_PASSWORD || "bohio2025",
-    database: process.env.DB_NAME || "inmobiliariabohio.cloudpepper.site",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     ssl: false,
   },
   useNullAsDefault: true,
